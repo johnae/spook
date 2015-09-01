c5726f9bc561fad229cec6e9f500a802bf499c4a
John Axel Eriksson <john@insane.se>
2015-09-01 17:43:10 +0200

    Fix OS X builds on travis

---
+++ ci/before_script.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+if [ "$TRAVIS_OS_NAME" = 'linux' ]; then
+  add-apt-repository --yes ppa:kalakris/cmake
+  apt-get update -qq
+  apt-get install cmake
+elif [ "$TRAVIS_OS_NAME" = 'osx' ]; then
+  brew update
+  brew install cmake
+else
+  echo "Unknown OS: $TRAVIS_OS_NAME"
+  exit 1
+fi
