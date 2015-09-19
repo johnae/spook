eb0d2fef681b6e5aeacb8e582687271cf1f8619a
John Axel Eriksson <john@insane.se>
2015-09-19 21:07:53 +0200

    Enable Spook self test iteratively - what Spook was made to do.

--- spec/support/run_busted.lua
+++ spec/support/run_busted.lua
@@ -4,8 +4,22 @@ _G.arg = {arg[#arg]}
 
 package.path = package.path .. ';spec/support/?.lua'
 package.path = package.path .. ';spec/support/?/init.lua'
+package.path = package.path .. ';lib/?.lua'
+package.path = package.path .. ';lib/?/init.lua'
 package.loaded.lfs = require('syscall.lfs')
+
+-- unload everything preloaded
+fs = require("fs")
+local entry, attr
+for entry, attr in fs.dirtree("lib", true) do
+  if entry:match("[^.].moon$") then
+    local hndl = (entry:gsub( "%.moon$", "" ):gsub( "^%./", "" ):gsub( "/", "." ):gsub( "\\", "." )):gsub( "%.init$", "" ):gsub("lib%.", "")
+    package.preload[hndl] = nil
+  end
+end
+
 local moonscript = require "moonscript.base"
+package.moonpath = moonscript.create_moonpath(package.path)
 local busted = assert(loadfile(base_dir .. '/busted/busted_bootstrap'))
 assert(moonscript.loadfile(base_dir .. '/../spec_helper.moon'))()
 busted()
