0e6799a4cb1af3b50fb22974081c1dd91d4136b5
John Axel Eriksson <john@insane.se>
2018-09-03 20:05:11 +0200

    Add glibcLocales to default.nix

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 
 stdenv.mkDerivation {
  name = "spook";
- buildInputs = [ gnumake gcc wget perl cacert tmux git ];
+ buildInputs = [ gnumake gcc wget perl cacert tmux git glibcLocales ];
  shellInit = ''
    export LC_ALL=en_US.UTF-8
    export LANG=en_US.UTF-8
