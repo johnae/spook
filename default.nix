650fc81ae5d053fb52080a4459803be64bbc1fc4
John Axel Eriksson <john@insane.se>
2018-09-03 11:13:22 +0200

    Add jenkinsfile and fix default.nix for CI.

--- default.nix
+++ default.nix
@@ -2,5 +2,10 @@ with import <nixpkgs> {};
 
 stdenv.mkDerivation {
  name = "spook";
- buildInputs = [ gnumake gcc wget perl cacert tmux git ];
+ buildInputs = [ gnumake gcc wget perl cacert tmux git glibcLocales ps gawk gnugrep ];
+ shellInit = ''
+   export LC_ALL=en_US.UTF-8
+   export LANG=en_US.UTF-8
+   export LANGUAGE=en_US.UTF-8
+ '';
 }
