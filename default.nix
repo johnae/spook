c7c2a6e0ca021dec9d696915a69bcc68845d0f6e
John Axel Eriksson <john@insane.se>
2018-09-03 20:18:16 +0200

    add other necessary dependencies to default.nix

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> {};
 
 stdenv.mkDerivation {
  name = "spook";
- buildInputs = [ gnumake gcc wget perl cacert tmux git glibcLocales ];
+ buildInputs = [ gnumake gcc wget perl cacert tmux git glibcLocales ps gawk gnugrep ];
  shellInit = ''
    export LC_ALL=en_US.UTF-8
    export LANG=en_US.UTF-8
