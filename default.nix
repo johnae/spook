832640b9f88d210662238982896125c8004f9668
John Axel Eriksson <john@insane.se>
2018-09-03 18:51:13 +0200

    Set locale for nix-shell

--- default.nix
+++ default.nix
@@ -3,4 +3,9 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
  name = "spook";
  buildInputs = [ gnumake gcc wget perl cacert tmux git ];
+ shellInit = ''
+   export LC_ALL=en_US.UTF-8
+   export LANG=en_US.UTF-8
+   export LANGUAGE=en_US.UTF-8
+ '';
 }
