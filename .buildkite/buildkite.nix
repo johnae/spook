e899f15b2c12fc1a22c3a53cf3480bb45b0db722
John Axel Eriksson <john@insane.se>
2019-04-13 23:36:32 +0200

    Buildkite: build without docker, improve pipeline generation.
    Stop relying on nix built scripts as they become an issue when
    building both Linux and Darwin. The actual build details should
    be handled outside of the pipeline generation anyway I think.
    
    This also means we don't need a pre-build hook atm.

--- .buildkite/buildkite.nix
+++ .buildkite/buildkite.nix
@@ -3,63 +3,23 @@ with lib;
 
 let
 
-  writeStrictShellScript = name: text:
-    writeTextFile {
-      inherit name;
-      executable = true;
-      text = ''
-        #!${stdenv.shell}
-        set -euo pipefail
-        ${text}
-      '';
-      checkPhase = ''
-        ## check the syntax
-        ${stdenv.shell} -n $out
-        ## shellcheck
-        ${shellcheck}/bin/shellcheck -e SC1117 -s bash -f tty $out
-      '';
-    };
-
   step = label: {
     command,
-    withPkgs ? [],
     environment ? [],
     agents ? [],
-    timeout ? 600,
-    volumes ? [ "/var/lib/buildkite/nix:/nix" ],
-    plugins ? [
-      {
-        "docker#v3.0.1" = {
-          image = builtins.getEnv "NIXIMAGE";
-          mount-buildkite-agent = false;
-          entrypoint = "/usr/bin/bash";
-          inherit environment volumes;
-        };
-      }
-    ]
+    timeout ? 600
     }:
       {
-        inherit label timeout plugins agents;
-        command = writeStrictShellScript
-         (toLower (
-           replaceStrings [" "] ["-"] (
-             replaceStrings [ ":" ] [""] (toString label)))) ''
-               export PATH=${if length withPkgs > 0 then
-               ''${makeSearchPath "bin" withPkgs }:$PATH''
-               else
-               ''$PATH''
-               }
-               ${command}
-             '';
+        inherit label timeout agents command;
       };
 
-   wait = "wait";
+  wait = "wait";
 
-   pipeline = s:
-     writeTextFile {
-      name = "pipeline.json";
-      text = builtins.toJSON (s);
-     };
+  pipeline = s:
+    writeTextFile {
+     name = "pipeline.json";
+     text = builtins.toJSON (s);
+    };
 
 
 in
