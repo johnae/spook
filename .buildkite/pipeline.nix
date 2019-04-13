f22fc57670002e73c1a9d672fc216e9b886d4df9
John Axel Eriksson <john@insane.se>
2019-04-13 23:36:32 +0200

    Buildkite: build without docker, improve pipeline generation.
    Stop relying on nix built scripts as they become an issue when
    building both Linux and Darwin. The actual build details should
    be handled outside of the pipeline generation anyway I think.
    
    This also means we don't need a pre-build hook atm.

--- .buildkite/pipeline.nix
+++ .buildkite/pipeline.nix
@@ -1,52 +1,65 @@
 with import ./buildkite.nix;
+with builtins;
+with lib;
 
 let
 
-  cache-steps = if builtins.getEnv "BUILDKITE_BRANCH" == "master" then
-  [
-    wait
-
-    (
-      step ":pipeline: Populate cachix cache" {
-        environment = [ "CACHIX_SIGNING_KEY" ];
-        agents = [ "queue=linux" "nix=true" ];
-        command = ''
-          echo --- Populate cachix cache
-          if [ -z "$CACHIX_SIGNING_KEY" ]; then
-             echo "Missing environment variable CACHIX_SIGNING_KEY"
-             exit 1
-          fi
-          nix-env -iA cachix -f https://cachix.org/api/v1/install
-          nix-store -qR --include-outputs "$(nix-instantiate build.nix)" | \
-          tee /dev/tty | \
-          cachix push insane
-        '';
-      }
-    )
-  ] else [];
+  ## build on macos as well when infra is in place
+  #run-on-agents = [["queue=linux" "nix=true"]
+  #          ["queue=macos" "nix=true"]];
+  run-on-agents = [[ "queue=linux" "nix=true" ]];
 
 in
 
-  pipeline ([
+  pipeline (
 
-    (step ":pipeline: Lint" {
-      agents = [ "queue=linux" "nix=true" ];
-      command = ''
-        nix-shell --run bash <<'NIXSH'
-          echo +++ Lint
-          make lint
-        NIXSH
-      '';
-    })
+   flatten (map (agents: [
+
+     (step ":pipeline: Lint" {
+       inherit agents;
+       command = ''
+         nix-shell --run bash <<'NIXSH'
+           echo +++ Lint
+           make lint
+         NIXSH
+       '';
+     })
 
     (step ":pipeline: Test" {
-      agents = [ "queue=linux" "nix=true" ];
-      command = ''
-        nix-shell --run bash <<'NIXSH'
-          echo +++ Test
-          make test
-        NIXSH
-      '';
+       inherit agents;
+       command = ''
+         nix-shell --run bash <<'NIXSH'
+           echo +++ Test
+           make test
+         NIXSH
+       '';
     })
 
-  ] ++ cache-steps)
+   ]
+
+   ++
+
+   (if getEnv "BUILDKITE_BRANCH" == "master" then
+     [
+       wait
+       (step ":pipeline: Populate cachix cache" {
+         inherit agents;
+         environment = [ "CACHIX_SIGNING_KEY" ];
+         command = ''
+           echo --- Populate cachix cache
+           if [ -z "$CACHIX_SIGNING_KEY" ]; then
+              echo "Missing environment variable CACHIX_SIGNING_KEY"
+              exit 1
+           fi
+           nix-env -iA cachix -f https://cachix.org/api/v1/install
+           nix-store -qR --include-outputs "$(nix-instantiate build.nix)" | \
+           tee /dev/tty | \
+           cachix push insane
+         '';
+         })
+     ]
+    else [])
+    )
+   run-on-agents ## map
+  )
+)
